76390b81644cfdff8f52f42ef568833d4bb0ebc7
sohaibfarooqi <sohaibfarooqi@hotmail.com>
2017-07-05 21:18:51 +0500

    renamed root component

--- reddit/index.js
+++ reddit/index.js
@@ -2,7 +2,7 @@ import 'babel-polyfill'
 
 import React from 'react'
 import { render } from 'react-dom'
-import Root from './containers/Root'
+import Root from './containers/root'
 
 render(
   <Root />,
