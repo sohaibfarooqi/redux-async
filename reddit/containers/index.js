e7ba96b71174cd2e64115f226ca4ee820008145f
sohaibfarooqi <sohaibfarooqi@hotmail.com>
2017-07-05 20:56:24 +0500

    container components

---
+++ reddit/containers/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+import configureStore from '../configureStore'
+import AsyncApp from './asyncApp'
+
+const store = configureStore()
+
+class Root extends Component{
+	render(){
+		<Provider store={store}>
+			<AsyncApp/>
+		</Provider>
+	}
+}
+
+export default Root
