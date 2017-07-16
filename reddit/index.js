861e84550e634f52432f4f7c2f6e42388ee76da5
sohaibfarooqi <sohaibfarooqi@hotmail.com>
2017-07-16 22:28:00 +0500

    deleting duplicate files

--- reddit/index.js
+++
@@ -1,9 +0,0 @@
-import 'babel-polyfill'
-import React from 'react'
-import {render} from 'react-dom'
-import Root from './containers/root'
-
-render(
-  <Root />,
-  document.getElementById('root')
-)
