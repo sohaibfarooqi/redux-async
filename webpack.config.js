9e3f40ea07441fc252f2453d4d920b808b30c58f
sohaibfarooqi <sohaibfarooqi@hotmail.com>
2017-07-16 22:24:13 +0500

    dev server config | production and development config seperate out

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,191 @@
-var webpack = require('webpack'),
-    HtmlWebpackPlugin = require('html-webpack-plugin'),
-    path = require('path');
+const webpack = require('webpack');
+const path = require('path');
+
+const DashboardPlugin = require('webpack-dashboard/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+
+const nodeEnv = process.env.NODE_ENV || 'development';
+const isProduction = nodeEnv === 'production';
+
+const jsSourcePath = path.join(__dirname, './reddit/js');
+const buildPath = path.join(__dirname, './build');
+const imgPath = path.join(__dirname, './reddit/assets/img');
+const iconPath = path.join(__dirname, './reddit/assets/icons');
+const sourcePath = path.join(__dirname, './reddit');
 
 
-module.exports = {
-  
-  entry: [
-    'webpack-hot-middleware/client?reload=true',
-    './reddit/index.js'
-  ],
+// Common plugins
+const plugins = [
+  new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    filename: 'vendor-[hash].js',
+    minChunks(module) {
+      const context = module.context;
+      return context && context.indexOf('node_modules') >= 0;
+    },
+  }),
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify(nodeEnv),
+    },
+  }),
+  new webpack.NamedModulesPlugin(),
+  new HtmlWebpackPlugin({
+    template: path.join(sourcePath, 'index.html'),
+    path: buildPath,
+    filename: 'index.html',
+  }),
+  new webpack.LoaderOptionsPlugin({
+    options: {
+      postcss: [
+        autoprefixer({
+          browsers: [
+            'last 3 version',
+            'ie >= 10',
+          ],
+        }),
+      ],
+      context: sourcePath,
+    },
+  }),
+];
 
-  output: {
-    path: path.join(__dirname, "dist"),
-    filename: "bundle.js", 
-  },
-
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loaders: ["babel-loader"]
-      },
-      {
-        test: /\.html$/,
-        loader: "file?name=[name].[ext]"
-      },
+// Common rules
+const rules = [
+  {
+    test: /\.(js|jsx)$/,
+    exclude: /node_modules/,
+    use: [
+      'babel-loader',
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
+  {
+    test: /\.svg$/,
+    use: [
+      {
+        loader: 'svg-sprite-loader',
+        options: {
+          extract: true,
+          spriteFilename: 'icons-sprite.svg',
+        },
+      },
+      'svgo-loader',
+    ],
+    include: iconPath,
+  },
+  {
+    test: /\.(png|gif|jpg|svg)$/,
+    include: imgPath,
+    use: 'url-loader?limit=20480&name=assets/[name]-[hash].[ext]',
+  },
+];
+
+if (isProduction) {
+  // Production plugins
+  plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true,
+      },
+      output: {
+        comments: false,
+      },
     }),
-    new webpack.optimize.OccurenceOrderPlugin(),
+    new ExtractTextPlugin('style-[hash].css')
+  );
+
+  // Production rules
+  rules.push(
+    {
+      test: /\.scss$/,
+      loader: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: 'css-loader!postcss-loader!sass-loader',
+      }),
+    }
+  );
+} else {
+  // Development plugins
+  plugins.push(
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development')
-    })
- ],
- resolve: {
-    extensions: ['', '.js']
-  }
+    new DashboardPlugin()
+  );
+
+  // Development rules
+  rules.push(
+    {
+      test: /\.scss$/,
+      exclude: /node_modules/,
+      use: [
+        'style-loader',
+        // Using source maps breaks urls in the CSS loader
+        // https://github.com/webpack/css-loader/issues/232
+        // This comment solves it, but breaks testing from a local network
+        // https://github.com/webpack/css-loader/issues/232#issuecomment-240449998
+        // 'css-loader?sourceMap',
+        'css-loader',
+        'postcss-loader',
+        'sass-loader?sourceMap',
+      ],
+    }
+  );
 }
+
+module.exports = {
+  devtool: isProduction ? false : 'source-map',
+  context: jsSourcePath,
+  entry: {
+    js: './index.js',
+  },
+  output: {
+    path: buildPath,
+    publicPath: '/',
+    filename: 'app-[hash].js',
+  },
+  module: {
+    rules,
+  },
+  resolve: {
+    extensions: ['.webpack-loader.js', '.web-loader.js', '.loader.js', '.js', '.jsx'],
+    modules: [
+      path.resolve(__dirname, 'node_modules'),
+      jsSourcePath,
+    ],
+  },
+  plugins,
+  devServer: {
+    contentBase: isProduction ? buildPath : sourcePath,
+    historyApiFallback: true,
+    port: 3000,
+    compress: isProduction,
+    inline: !isProduction,
+    hot: !isProduction,
+    host: '0.0.0.0',
+    disableHostCheck: true,
+    stats: {
+      assets: true,
+      children: false,
+      chunks: false,
+      hash: false,
+      modules: false,
+      publicPath: false,
+      timings: true,
+      version: false,
+      warnings: true,
+      colors: {
+        green: '\u001b[32m',
+      },
+    },
+  },
+};
